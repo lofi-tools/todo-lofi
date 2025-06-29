ea870dfa04a24a721353c0ac92add2ca358c20b1
Nicolas M <marshall.nicolas@gmail.com>
2025-06-29 21:02:02 +0100

    fix flake: still rust issues

--- flake.nix
+++ flake.nix
@@ -3,59 +3,49 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    crane = {      url = "github:ipetkov/crane";      inputs.nixpkgs.follows = "nixpkgs";    };
-    rust-overlay = {      url = "github:oxalica/rust-overlay";      inputs.nixpkgs.follows = "nixpkgs";      inputs.flake-utils.follows = "flake-utils";    };
+    crane = { url = "github:ipetkov/crane"; inputs.nixpkgs.follows = "nixpkgs"; };
+    rust-overlay = { url = "github:oxalica/rust-overlay"; inputs.nixpkgs.follows = "nixpkgs"; inputs.flake-utils.follows = "flake-utils"; };
   };
 
   outputs = inputs@{ self, nixpkgs, flake-utils, flake-parts, crane, rust-overlay, ... }:
-    flake-parts.lib.mkFlake { inherit self; } {
+    with builtins; flake-parts.lib.mkFlake { inherit inputs; } {
       systems = flake-utils.lib.defaultSystems;
       imports = [ ]; # Add any flake-parts modules here if needed
 
-      perSystem = { config, pkgs, system, ... }:
+      perSystem = { config, pkgs, system, lib, ... }:
         let
-          overlays = [ (import rust-overlay) ];
-          # pkgs' = import nixpkgs {
-          #   inherit system overlays;
-          # };
-          rustToolchain = pkgs.rust-bin.stable.latest.default.override {
-            extensions = [ "rust-src" ];
+          ownPkgs = {
+            rust = pkgs.rust-bin.stable.latest.default.override {
+              extensions = [ "rust-src" ];
+            };
           };
-          craneLib = crane.lib.${pkgs.system}.overrideToolchain rustToolchain;
 
-          # Build the workspace members
-          src = pkgs.lib.cleanSource ./.;
+          craneLib = (crane.mkLib pkgs).overrideToolchain ownPkgs.rust;
           commonArgs = {
-            inherit src;
-            nativeBuildInputs = with pkgs; [ pkg-config ]; # Add native build dependencies here
+            src = pkgs.lib.cleanSource ./.;
+            pname = "todo-lofi";
+            nativeBuildInputs = buildDeps; # Add native build dependencies here
           };
-
-          # Build the cargo artifacts
           cargoArtifacts = craneLib.buildDepsOnly commonArgs;
 
-          # Build the todo-core crate
           todo-core = craneLib.buildPackage (commonArgs // {
             inherit cargoArtifacts;
             pname = "todo-core";
           });
 
-        in
-        {
-          # Development shell
-          devShells.default = pkgs.mkShell {
-            inputsFrom = [ cargoArtifacts ];
-            nativeBuildInputs = with pkgs; [
-              rustToolchain
-              cargo-watch # Optional: for development workflow
-              pkg-config
-              # Add other dev tools here
-            ];
-            # Add environment variables if needed
-            # RUST_SRC_PATH = "${rustToolchain}/lib/rustlib/src/rust/library";
+          buildDeps = [ ownPkgs.rust pkgs.pkg-config ];
+          devDeps = [ pkgs.cargo-watch ];
+
+          scripts = mapAttrs (name: txt: pkgs.writeShellScriptBin name txt) {
+            prun = ''set -x; package="$1"; shift; cargo run -p "$package" -- $@'';
           };
 
-          # Packages (optional, if you want to package binaries)
-          # packages.default = todo-core; # Or specific binary crate
+        in
+        {
+          devShells.default = pkgs.mkShell {
+            inputsFrom = [ cargoArtifacts ];
+            buildInputs = buildDeps ++ devDeps ++ attrValues scripts;
+          };
 
           # Checks: build and test
           checks = {
@@ -67,7 +57,12 @@
               packageFilter = [ "todo-core" ]; # Specify the package to test
               doCheck = true;
             });
-            # Add checks for other crates if needed
+          };
+
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [ (import rust-overlay) ];
+            config = { };
           };
         };
     };
