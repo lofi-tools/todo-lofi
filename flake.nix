ea4a2c5328188cbc2b909da0665190f317bd5045
Nicolas M <marshall.nicolas@gmail.com>
2026-02-11 16:49:29 +0000

    gpui: flake deps + hello-world

--- flake.nix
+++ flake.nix
@@ -7,12 +7,18 @@
   inputs.parts.url = "github:hercules-ci/flake-parts";
   inputs.my-nix = { url = "github:nmrshll/nix-utils"; inputs.nixpkgs.follows = "nixpkgs"; inputs.fp.follows = "parts"; };
 
+  # inputs.exo.url = "github:exo-explore/exo";
+  # nixConfig = {
+  #   extra-trusted-public-keys = "exo.cachix.org-1:okq7hl624TBeAR3kV+g39dUFSiaZgLRkLsFBCuJ2NZI=";
+  #   extra-substituters = "https://exo.cachix.org";
+  # };
+
 
   outputs = inputs@{ self, parts, my-nix, ... }: parts.lib.mkFlake { inherit inputs; } (top@{ lib, ... }:
     with builtins; {
       systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
       imports = (attrValues inputs.my-nix.flakeModules) ++ [ ];
-      perSystem = { pkgs, system, lib, l, self', ... }:
+      perSystem = { pkgs, system, lib, l, self', ownPkgs, ... }:
         let
           bin = inputs.my-nix.bin.${system} // (mapAttrs (n: p: "${p}/bin/${n}") scripts);
 
@@ -20,54 +26,66 @@
             pkgs.pkg-config
           ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
             pkgs.libiconv
-            # pkgs.darwin.apple_sdk.frameworks.Security
-            # pkgs.darwin.apple_sdk.frameworks.CoreServices
-            # pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
-            # pkgs.darwin.apple_sdk.frameworks.WebKit
-            # pkgs.darwin.apple_sdk.frameworks.AppKit
+            # inputs.exo.packages.${system}.metal-toolchain
+            # pkgs.darwin.xcode_26_1_Apple_silicon # comment out for first build
+            # pkgs.apple-sdk_26
+            # TODO try symlinkJoin of xcode and exo.metal-toolchain
+            ownPkgs.install-xcode-global
           ] ++ pkgs.lib.optionals pkgs.stdenv.isLinux [
-            # pkgs.webkitgtk
-            # pkgs.gtk3
-            # pkgs.cairo
-            # pkgs.gdk-pixbuf
-            # pkgs.glib
-            # pkgs.dbus
-            # pkgs.openssl_3
-            # pkgs.librsvg
-            # pkgs.libsoup_3
+            /*  pkgs.webkitgtk */
+            /*  pkgs.gtk3 */
+            /*  pkgs.cairo */
+            /*  pkgs.gdk-pixbuf */
+            /*  pkgs.glib */
+            /*  pkgs.dbus */
+            /*  pkgs.openssl_3 */
+            /*  pkgs.librsvg */
+            /*  pkgs.libsoup_3 */
           ];
+
           devDeps = [ pkgs.cargo-tauri pkgs.cargo-watch ];
 
           wd = "$(git rev-parse --show-toplevel)";
-          scripts = mapAttrs (n: s: pkgs.writeShellScriptBin n s) { };
+          scripts = mapAttrs (n: s: pkgs.writeShellScriptBin n s) {
+            xcrun = ''${env.DEVELOPER_DIR}/Contents/Developer/usr/bin/xcrun'';
+            # metal = ''${env.DEVELOPER_DIR}/Toolchains/XcodeDefault.xctoolchain/usr/bin/metal $@'';
+            dbg-env = '' ${concatStringsSep "\n" (attrValues (mapAttrs (n: v: "printf \"${n}=${v}\\n\"") env))} '';
+            dbg-store-xcode = '' DEVELOPER_DIR="${ownPkgs.install-xcode-global.DEV_DIR}" xcodebuild -version '';
+          };
 
+          env = {
+            # DEVELOPER_DIR = "${unsafeDiscardStringContext pkgs.darwin.xcode_26_1_Apple_silicon}/Contents/Developer";
+            # SDKROOT = "${env.DEVELOPER_DIR}/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk";
+            # SDKROOT = "${inputs.exo.packages.${system}.metal-toolchain}";
+            # METAL = "${inputs.exo.packages.${system}.metal-toolchain}/bin/metal";
+            # BINDGEN_EXTRA_CLANG_ARGS = "-I${inputs.exo.packages.${system}.metal-toolchain}";
 
-
-          # # TODO move to my-nix
-          # workspaceMembers = (fromTOML (readFile ./Cargo.toml)).workspace.members or [ ];
-          # expandWsMember = member:
-          #   if lib.strings.hasSuffix "/*" member then
-          #     let
-          #       baseDir = lib.strings.removeSuffix "/*" member;
-          #       subDirs = lib.filterAttrs (name: type: type == "directory") (readDir (./. + "/${baseDir}"));
-          #     in
-          #     map (name: "${baseDir}/${name}") (attrNames subDirs)
-          #   else
-          #     [ member ];
-
-          # validCratePaths = filter (crate: pathExists (./. + "/${crate}/Cargo.toml")) (concatLists (map expandWsMember workspaceMembers));
-          # crates = l.listToAttrs (map (path: { name = baseNameOf path; value = l.customRust.buildCrate path; }) validCratePaths);
-
-
+            DEVELOPER_DIR = ownPkgs.install-xcode-global.DEV_DIR;
+            SDKROOT = ownPkgs.install-xcode-global.SDKROOT;
+          };
 
         in
         {
           # packages = scripts;
-          # checks = tests;
-          devShellParts.env = { };
+          rust.buildInputs = buildDeps;
+          rust.buildEnv = env;
+
+          devShellParts.env = env;
+          devShellParts.shellHookParts = {
+            install-xcode = '' ${ownPkgs.install-xcode-global}/bin/install-xcode-global '';
+            # install-metal = ''xcodebuild -importComponent metalToolchain -importPath .cache/Metal.dmg'';
+            # use-os-xcrun = ''export PATH="/usr/bin:/usr/local/bin:$PATH"'';
+            #     DEVELOPER_DIR = unsafeDiscardStringContext DEV_DIR;
+            # SDKROOT = unsafeDiscardStringContext "${DEV_DIR}/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk";
+            appleSdkDirs = '' 
+              export DEVELOPER_DIR="${env.DEVELOPER_DIR}"
+              export SDKROOT="${env.SDKROOT}"
+            '';
+          };
           devShellParts.buildInputs = buildDeps ++ devDeps ++ (attrValues scripts);
         };
     });
+
 }
 
 #   outputs = inputs@{ self, nixpkgs, flake-utils, flake-parts, crane, rust-overlay, ... }:
