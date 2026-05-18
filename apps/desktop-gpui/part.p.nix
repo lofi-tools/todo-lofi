3d6a711d3dcf9240488699848549773fddea0461
Nicolas M <marshall.nicolas@gmail.com>
2026-05-18 12:15:03 +0100

    fix: build metal

--- apps/desktop-gpui/part.p.nix
+++ apps/desktop-gpui/part.p.nix
@@ -1,19 +1,23 @@
-{ l, config, ... }: {
+{ l, ... }: {
   perSystem = { pkgs, ... }: with builtins; let
-    # adding rust from rust-overlay auto-patches CC, makes xcodebuild/metal fail:
-    # solution: devShells.default = pkgs.mkShell.override { stdenv = customStdenv; }
-    customRust = pkgs.rust-bin.stable.latest.default.override {
-      extensions = [ "rust-src" "rust-analyzer" ];
-      targets = [ ];
-    };
-    craneLib = config.flakeInputsOf.my-nix.crane.mkLib pkgs;
-
-
     # bin = (mapAttrs (n: p: "${p}/bin/${n}") scripts);
-    buildTimeDeps = [
+    buildDeps = [
       # pkgs.pkg-config
-      customRust
-      pkgs.cowsay
+      # inputs.exo.packages.${system}.metal-toolchain
+      # pkgs.darwin.xcode_26_1_Apple_silicon # comment out for first build
+      # pkgs.apple-sdk_26
+      # TODO try symlinkJoin of xcode and exo.metal-toolchain
+      # pkgs.own.my-nix.install-xcode-global
+    ] ++ pkgs.lib.optionals pkgs.stdenv.isLinux [
+      /*  pkgs.webkitgtk */
+      /*  pkgs.gtk3 */
+      /*  pkgs.cairo */
+      /*  pkgs.gdk-pixbuf */
+      /*  pkgs.glib */
+      /*  pkgs.dbus */
+      /*  pkgs.openssl_3 */
+      /*  pkgs.librsvg */
+      /*  pkgs.libsoup_3 */
     ];
     runtimeDeps = [
       # pkgs.openssl
@@ -26,6 +30,8 @@
       pdg = "cargo run -p desktop-gpui";
       dbg-env = '' ${concatStringsSep "\n" (attrValues (mapAttrs (n: v: "printf \"${n}=${v}\\n\"") env))} '';
       dbg-store-xcode = '' DEVELOPER_DIR="${pkgs.own.my-nix.install-xcode-global.DEV_DIR}" xcodebuild -version '';
+      # xcrun = ''${env.DEVELOPER_DIR}/Contents/Developer/usr/bin/xcrun'';
+      # metal = ''${env.DEVELOPER_DIR}/Toolchains/XcodeDefault.xctoolchain/usr/bin/metal $@'';
     };
 
     env = {
@@ -34,18 +40,40 @@
       # NIX_LDFLAGS = "-F/System/Library/Frameworks -F/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/System/Library/Frameworks";
       # # Tells the compiler where to find the headers if a build script (-sys crate) compiles C/C++ code
       # BINDGEN_EXTRA_CLANG_ARGS = "-F/System/Library/Frameworks";
+
+      # DEVELOPER_DIR = "${unsafeDiscardStringContext pkgs.darwin.xcode_26_1_Apple_silicon}/Contents/Developer";
+      # SDKROOT = "${env.DEVELOPER_DIR}/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk";
+      # SDKROOT = "${inputs.exo.packages.${system}.metal-toolchain}";
+      # METAL = "${inputs.exo.packages.${system}.metal-toolchain}/bin/metal";
+      # BINDGEN_EXTRA_CLANG_ARGS = "-I${inputs.exo.packages.${system}.metal-toolchain}";
     };
   in
   {
     packages = scripts;
-    pkgs.overlays = [ (import config.flakeInputsOf.my-nix.rust-overlay) ];
-    # rust.buildInputs = runtimeDeps;
-    # rust.nativeBuildInputs = buildTimeDeps;
-    # rust.buildEnv = env;
-    devShellParts.env = env;
-    devShellParts.buildInputs = buildTimeDeps ++ runtimeDeps ++ devDeps ++ (attrValues scripts);
-    devShellParts.shellHookParts = {
+    # pkgs.overlays = [ (import config.flakeInputsOf.my-nix.rust-overlay) ];
+    rust.buildInputs = runtimeDeps;
+    rust.nativeBuildInputs = buildDeps;
+    rust.buildEnv = env;
+    myDevShell.env = env;
+    myDevShell.buildInputs = buildDeps ++ runtimeDeps ++ devDeps ++ (attrValues scripts);
+    myDevShell.shellHooks = {
       configure_C = l.concatStringsSep "\n" (l.mapAttrsToList (name: value: "export ${name}=\"${value}\"") env);
+      # install-xcode = '' ${pkgs.own.my-nix.install-xcode-global}/bin/install-xcode-global '';
+      # # install-metal = ''xcodebuild -importComponent metalToolchain -importPath .cache/Metal.dmg'';
+      # # use-os-xcrun = ''export PATH="/usr/bin:/usr/local/bin:$PATH"'';
+      # #     DEVELOPER_DIR = unsafeDiscardStringContext DEV_DIR;
+      # # SDKROOT = unsafeDiscardStringContext "${DEV_DIR}/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk";
+      # force-sdk-dirs = ''
+      #   export DEVELOPER_DIR="${env.DEVELOPER_DIR}"
+      #   export SDKROOT="${env.SDKROOT}"
+      #   export MACOSX_DEPLOYMENT_TARGET="15.0"
+      #   # Explicitly expose the global metal toolchain binary directly to the path
+      #   export PATH="/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin:$PATH"
+      #   export TOOLCHAINS=default
+      # '';
     };
+    # adding rust from rust-overlay auto-patches CC, makes xcodebuild/metal fail:
+    # solution: devShells.default = pkgs.mkShell.override { stdenv = customStdenv; }
+    myDevShell.overrides.stdenv = pkgs.stdenvNoCC;
   };
 }
--- flake.nix
+++ flake.nix
@@ -1,230 +1,61 @@
 {
   # inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  # inputs.flake-utils.url = "github:numtide/flake-utils";
-  # inputs.crane = { url = "github:ipetkov/crane"; };
-  # inputs.rust-overlay = { url = "github:oxalica/rust-overlay"; inputs.nixpkgs.follows = "nixpkgs"; };
   inputs.nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
   inputs.parts.url = "github:hercules-ci/flake-parts";
   inputs.my-nix = { url = "github:nmrshll/nix-utils"; inputs.nixpkgs.follows = "nixpkgs"; inputs.fp.follows = "parts"; };
-
   # inputs.exo.url = "github:exo-explore/exo";
+
   # nixConfig = {
   #   extra-trusted-public-keys = "exo.cachix.org-1:okq7hl624TBeAR3kV+g39dUFSiaZgLRkLsFBCuJ2NZI=";
   #   extra-substituters = "https://exo.cachix.org";
   # };
 
 
-  outputs = inputs@{ self, parts, my-nix, ... }: parts.lib.mkFlake { inherit inputs; } (top@{ lib, ... }:
+  outputs = inputs@{ self, parts, my-nix, ... }: parts.lib.mkFlake { inherit inputs; } ({ lib, ... }:
     with builtins; {
       systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
       imports = lib.flatten [
         (attrValues inputs.my-nix.flakeModules.essentials)
-        # inputs.my-nix.flakeModules.rust
+        inputs.my-nix.flakeModules.rust
         (inputs.my-nix.lib.findFlakePartFilesRec ./.)
       ];
       perSystem = { pkgs, ... }:
         let
           # bin = inputs.my-nix.bin.${system} // (mapAttrs (n: p: "${p}/bin/${n}") scripts);
-
           buildDeps = [
             pkgs.pkg-config
-          ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
-            # pkgs.libiconv
-            # inputs.exo.packages.${system}.metal-toolchain
-            # pkgs.darwin.xcode_26_1_Apple_silicon # comment out for first build
-            # pkgs.apple-sdk_26
-            # TODO try symlinkJoin of xcode and exo.metal-toolchain
-            # pkgs.own.my-nix.install-xcode-global
-          ] ++ pkgs.lib.optionals pkgs.stdenv.isLinux [
-            /*  pkgs.webkitgtk */
-            /*  pkgs.gtk3 */
-            /*  pkgs.cairo */
-            /*  pkgs.gdk-pixbuf */
-            /*  pkgs.glib */
-            /*  pkgs.dbus */
-            /*  pkgs.openssl_3 */
-            /*  pkgs.librsvg */
-            /*  pkgs.libsoup_3 */
           ];
 
           devDeps = [ pkgs.cargo-tauri pkgs.cargo-watch ];
 
           # wd = "$(git rev-parse --show-toplevel)";
-          scripts = mapAttrs (n: s: pkgs.writeShellScriptBin n s) { };
-
-          env = {
-            # DEVELOPER_DIR = pkgs.own.my-nix.install-xcode-global.DEV_DIR;
-            # SDKROOT = pkgs.own.my-nix.install-xcode-global.SDKROOT;
-            # NIX_LDFLAGS = "-F/System/Library/Frameworks -F/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/System/Library/Frameworks";
-            # # Tells the compiler where to find the headers if a build script (-sys crate) compiles C/C++ code
-            # BINDGEN_EXTRA_CLANG_ARGS = "-F/System/Library/Frameworks";
+          scripts = mapAttrs (n: s: pkgs.writeShellScriptBin n s) {
+            # prun = ''set -x; package="$1"; shift; cargo run -p "$package" -- $@'';
+            dt = ''set -e;  cd desktop; cargo tauri dev '';
           };
+
+          env = { };
+
+          # checks = {
+          #   # Check that todo-core builds
+          #   inherit todo-core;
+          #   # Check that todo-core tests pass
+          #   todo-core-tests = craneLib.cargoNextest (commonArgs // {
+          #     inherit cargoArtifacts;
+          #     packageFilter = [ "todo-core" ]; # Specify the package to test
+          #     doCheck = true;
+          #   });
+          # };
 
         in
         {
           # packages = scripts;
           # rust.buildInputs = buildDeps;
           # rust.buildEnv = env;
-          devShellParts.env = env;
-          devShellParts.shellHookParts = { };
-          devShellParts.buildInputs = buildDeps ++ devDeps ++ (attrValues scripts);
+          myDevShell.env = env;
+          myDevShell.shellHooks = { };
+          myDevShell.buildInputs = buildDeps ++ devDeps ++ (attrValues scripts);
         };
     });
 
 }
-
-# perSystem = { pkgs, ... }:
-#   let
-#     # bin = inputs.my-nix.bin.${system} // (mapAttrs (n: p: "${p}/bin/${n}") scripts);
-
-#     buildDeps = [
-#       # pkgs.pkg-config
-#     ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
-#       # pkgs.libiconv
-#       # inputs.exo.packages.${system}.metal-toolchain
-#       # pkgs.darwin.xcode_26_1_Apple_silicon # comment out for first build
-#       # pkgs.apple-sdk_26
-#       # TODO try symlinkJoin of xcode and exo.metal-toolchain
-#       # pkgs.own.my-nix.install-xcode-global
-#     ] ++ pkgs.lib.optionals pkgs.stdenv.isLinux [
-#       /*  pkgs.webkitgtk */
-#       /*  pkgs.gtk3 */
-#       /*  pkgs.cairo */
-#       /*  pkgs.gdk-pixbuf */
-#       /*  pkgs.glib */
-#       /*  pkgs.dbus */
-#       /*  pkgs.openssl_3 */
-#       /*  pkgs.librsvg */
-#       /*  pkgs.libsoup_3 */
-#     ];
-
-#     devDeps = [ pkgs.cargo-tauri pkgs.cargo-watch ];
-
-#     wd = "$(git rev-parse --show-toplevel)";
-#     scripts = mapAttrs (n: s: pkgs.writeShellScriptBin n s) {
-#       # xcrun = ''${env.DEVELOPER_DIR}/Contents/Developer/usr/bin/xcrun'';
-#       # metal = ''${env.DEVELOPER_DIR}/Toolchains/XcodeDefault.xctoolchain/usr/bin/metal $@'';
-#       dbg-env = '' ${concatStringsSep "\n" (attrValues (mapAttrs (n: v: "printf \"${n}=${v}\\n\"") env))} '';
-#       dbg-store-xcode = '' DEVELOPER_DIR="${pkgs.own.my-nix.install-xcode-global.DEV_DIR}" xcodebuild -version '';
-#     };
-
-#     env = {
-#       # DEVELOPER_DIR = "${unsafeDiscardStringContext pkgs.darwin.xcode_26_1_Apple_silicon}/Contents/Developer";
-#       # SDKROOT = "${env.DEVELOPER_DIR}/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk";
-#       # SDKROOT = "${inputs.exo.packages.${system}.metal-toolchain}";
-#       # METAL = "${inputs.exo.packages.${system}.metal-toolchain}/bin/metal";
-#       # BINDGEN_EXTRA_CLANG_ARGS = "-I${inputs.exo.packages.${system}.metal-toolchain}";
-
-#       DEVELOPER_DIR = pkgs.own.my-nix.install-xcode-global.DEV_DIR;
-#       SDKROOT = pkgs.own.my-nix.install-xcode-global.SDKROOT;
-#     };
-
-#   in
-#   {
-#     # packages = scripts;
-#     rust.buildInputs = buildDeps;
-#     rust.buildEnv = env;
-
-#     devShellParts.env = env;
-#     devShellParts.shellHookParts = {
-#       install-xcode = '' ${pkgs.own.my-nix.install-xcode-global}/bin/install-xcode-global '';
-#       # install-metal = ''xcodebuild -importComponent metalToolchain -importPath .cache/Metal.dmg'';
-#       # use-os-xcrun = ''export PATH="/usr/bin:/usr/local/bin:$PATH"'';
-#       #     DEVELOPER_DIR = unsafeDiscardStringContext DEV_DIR;
-#       # SDKROOT = unsafeDiscardStringContext "${DEV_DIR}/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk";
-#       force-sdk-dirs = ''
-#         export DEVELOPER_DIR="${env.DEVELOPER_DIR}"
-#         export SDKROOT="${env.SDKROOT}"
-#         export MACOSX_DEPLOYMENT_TARGET="15.0"
-#         # Explicitly expose the global metal toolchain binary directly to the path
-#         export PATH="/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin:$PATH"
-#         export TOOLCHAINS=default
-#       '';
-#     };
-#     devShellParts.buildInputs = buildDeps ++ devDeps ++ (attrValues scripts);
-#   };
-
-
-
-#   outputs = inputs@{ self, nixpkgs, flake-utils, flake-parts, crane, rust-overlay, ... }:
-#     with builtins; flake-parts.lib.mkFlake { inherit inputs; } {
-#       systems = flake-utils.lib.defaultSystems;
-#       imports = [ ]; # Add any flake-parts modules here if needed
-
-#       perSystem = { config, pkgs, system, lib, ... }:
-#         let
-#           ownPkgs = {
-#             rust = pkgs.rust-bin.stable.latest.default.override {
-#               extensions = [ "rust-src" ];
-#             };
-#           };
-
-#           craneLib = (crane.mkLib pkgs).overrideToolchain ownPkgs.rust;
-#           commonArgs = {
-#             src = pkgs.lib.cleanSource ./.;
-#             pname = "todo-lofi";
-#             nativeBuildInputs = buildDeps; # Add native build dependencies here
-#           };
-#           cargoArtifacts = craneLib.buildDepsOnly commonArgs;
-
-#           todo-core = craneLib.buildPackage (commonArgs // {
-#             inherit cargoArtifacts;
-#             pname = "todo-core";
-#           });
-
-#           buildDeps = [
-#             ownPkgs.rust
-#             pkgs.pkg-config
-#           ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
-#             pkgs.darwin.apple_sdk.frameworks.Security
-#             pkgs.darwin.apple_sdk.frameworks.CoreServices
-#             pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
-#             pkgs.darwin.apple_sdk.frameworks.WebKit
-#             pkgs.darwin.apple_sdk.frameworks.AppKit
-#           ] ++ pkgs.lib.optionals pkgs.stdenv.isLinux [
-#             pkgs.webkitgtk
-#             pkgs.gtk3
-#             pkgs.cairo
-#             pkgs.gdk-pixbuf
-#             pkgs.glib
-#             pkgs.dbus
-#             pkgs.openssl_3
-#             pkgs.librsvg
-#             pkgs.libsoup_3
-#           ];
-#           devDeps = [ pkgs.cargo-tauri pkgs.cargo-watch ];
-
-#           scripts = mapAttrs (name: txt: pkgs.writeShellScriptBin name txt) {
-#             prun = ''set -x; package="$1"; shift; cargo run -p "$package" -- $@'';
-#             dt = ''set -e
-#               cd desktop; cargo tauri dev
-#             '';
-#           };
-
-#         in
-#         {
-#           devShells.default = pkgs.mkShell {
-#             inputsFrom = [ cargoArtifacts ];
-#             buildInputs = buildDeps ++ devDeps ++ attrValues scripts;
-#           };
-
-#           # Checks: build and test
-#           checks = {
-#             # Check that todo-core builds
-#             inherit todo-core;
-#             # Check that todo-core tests pass
-#             todo-core-tests = craneLib.cargoNextest (commonArgs // {
-#               inherit cargoArtifacts;
-#               packageFilter = [ "todo-core" ]; # Specify the package to test
-#               doCheck = true;
-#             });
-#           };
-
-#           _module.args.pkgs = import inputs.nixpkgs {
-#             inherit system;
-#             overlays = [ (import rust-overlay) ];
-#             config = { };
-#           };
-#         };
-#     };
-# }
